b37006fb758caa08e5e8d00007f368cda0f83688
Caskillerlol <casdonderwinkel2@gmail.com>
2025-04-14 16:36:46 +0200

    Ground Collision Works Propperly
    Now you can stand on blocks and you fall if there is nothing beneath you. Other collision still needs work

--- Game/player.gd
+++ Game/player.gd
@@ -1,38 +1,50 @@
 extends Area2D
 signal hit
-@export var speed = 600
+@export var speed = 200
 var x_velocity = 0
 var y_velocity = 0
 var x_acceleration = 0
 var y_acceleration = 0
-var screen_size
 var collision_radius
-var touching_ground
+var raycast_collision = 100000
+var screen_size
+var touching_ground = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	collision_radius = $CollisionShape2D.shape.size.y / 2
-	
+
+
+#returns the maximum/minimum value that isn't zero
+func compare_non_zero(a, b, mode):
+	if a == 0:
+		return b
+	if b == 0:
+		return a
+	if mode == "min":
+		return min(a, b)
+	if mode == "max":
+		return max(a, b)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
 	x_velocity = 0
-	y_velocity = 0
 	if Input.is_action_pressed("move_right"):
 		x_velocity += 1
 	if Input.is_action_pressed("move_left"):
 		x_velocity -= 1
 	if Input.is_action_pressed("move_up"):
 		if touching_ground:
-			y_acceleration = -40
+			y_acceleration = -10
+			y_velocity = -30
 	if Input.is_action_pressed("move_down"):
 		y_velocity += 1
-	
-	#Creates the raycast and checks for collision
-	var space_state = get_world_2d().direct_space_state
-	var query = PhysicsRayQueryParameters2D.create(Vector2(position.x, position.y), Vector2(position.x, position.y + 400))
-	var result = space_state.intersect_ray(query)
-	
+
+
+func _physics_process(delta) -> void:
 	#gravity
 	if !touching_ground:
 		y_acceleration += 1
@@ -41,20 +53,28 @@ func _process(delta) -> void:
 	x_velocity += x_acceleration * delta
 	y_velocity += y_acceleration * delta
 	
-	
 	#moves the player
-	position.x += x_velocity * speed * delta
-	position.y += y_velocity * speed * delta
-	if result:
-		position.y = min(position.y, result.position.y)
-		if position.y == result.position.y:
+	position.x += x_velocity * delta * speed
+	position.y += y_velocity * delta
+	
+	#Checks for raycast hit
+	if $RayCast2D_DownLeft.is_colliding() or $RayCast2D_DownRight.is_colliding():
+		raycast_collision = compare_non_zero($RayCast2D_DownLeft.get_collision_point().y, $RayCast2D_DownRight.get_collision_point().y, "min")
+		#Pushes players out of collision boxes
+		if position.y + collision_radius >= raycast_collision:
+			position.y = raycast_collision - collision_radius
 			touching_ground = true
 			y_acceleration = 0
+			y_velocity = 0
 		else:
 			touching_ground = false
 	else:
 		touching_ground = false
-	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	#checks if player goes below screen
+	if position.y + collision_radius >= screen_size.y:
+		pass
+		print("dead")
 
 
 func _on_body_entered(body: Node2D) -> void:
