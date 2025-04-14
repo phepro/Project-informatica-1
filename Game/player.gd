55e359373bb548edb07b0adb69f7dfece18bba93
Caskillerlol <casdonderwinkel2@gmail.com>
2025-04-14 19:00:08 +0200

    Finished player movement
    Now there is collision on all sides.
    Everything works PERFECTLY

--- Game/player.gd
+++ Game/player.gd
@@ -1,12 +1,11 @@
 extends Area2D
 signal hit
-@export var speed = 200
+@export var speed = 400
 var x_velocity = 0
 var y_velocity = 0
-var x_acceleration = 0
-var y_acceleration = 0
-var collision_radius
-var raycast_collision = 100000
+var collision_height
+var collision_width
+var raycast_collision
 var screen_size
 var touching_ground = false
 
@@ -14,7 +13,8 @@ var touching_ground = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	collision_radius = $CollisionShape2D.shape.size.y / 2
+	collision_height = $CollisionShape2D.shape.size.y / 2
+	collision_width = $CollisionShape2D.shape.size.x / 2
 
 
 #returns the maximum/minimum value that isn't zero
@@ -38,8 +38,7 @@ func _process(delta) -> void:
 		x_velocity -= 1
 	if Input.is_action_pressed("move_up"):
 		if touching_ground:
-			y_acceleration = -10
-			y_velocity = -30
+			y_velocity = -640
 	if Input.is_action_pressed("move_down"):
 		y_velocity += 1
 
@@ -47,24 +46,29 @@ func _process(delta) -> void:
 func _physics_process(delta) -> void:
 	#gravity
 	if !touching_ground:
-		y_acceleration += 1
-	
-	#calculates velocity
-	x_velocity += x_acceleration * delta
-	y_velocity += y_acceleration * delta
+		y_velocity += 1280 * delta
 	
 	#moves the player
 	position.x += x_velocity * delta * speed
 	position.y += y_velocity * delta
 	
 	#Checks for raycast hit
-	if $RayCast2D_DownLeft.is_colliding() or $RayCast2D_DownRight.is_colliding():
-		raycast_collision = compare_non_zero($RayCast2D_DownLeft.get_collision_point().y, $RayCast2D_DownRight.get_collision_point().y, "min")
+	if $RayCast2D_Left1.is_colliding() or $RayCast2D_Left2.is_colliding():
+		raycast_collision = compare_non_zero($RayCast2D_Left1.get_collision_point().x, $RayCast2D_Left2.get_collision_point().x, "max")
+		position.x = max(position.x, raycast_collision + collision_width)
+	if $RayCast2D_Right1.is_colliding() or $RayCast2D_Right2.is_colliding():
+		raycast_collision = compare_non_zero($RayCast2D_Right1.get_collision_point().x, $RayCast2D_Right2.get_collision_point().x, "min")
+		position.x = min(position.x, raycast_collision - collision_width)
+	if $RayCast2D_Up1.is_colliding() or $RayCast2D_Up2.is_colliding():
+		raycast_collision = compare_non_zero($RayCast2D_Up1.get_collision_point().y, $RayCast2D_Up2.get_collision_point().y, "max")
+		position.y = max(position.y, raycast_collision + collision_height)
+	
+	if $RayCast2D_Down1.is_colliding() or $RayCast2D_Down2.is_colliding():
+		raycast_collision = compare_non_zero($RayCast2D_Down1.get_collision_point().y, $RayCast2D_Down2.get_collision_point().y, "min")
 		#Pushes players out of collision boxes
-		if position.y + collision_radius >= raycast_collision:
-			position.y = raycast_collision - collision_radius
+		if position.y + collision_height >= raycast_collision:
+			position.y = raycast_collision - collision_height
 			touching_ground = true
-			y_acceleration = 0
 			y_velocity = 0
 		else:
 			touching_ground = false
@@ -72,7 +76,7 @@ func _physics_process(delta) -> void:
 		touching_ground = false
 	
 	#checks if player goes below screen
-	if position.y + collision_radius >= screen_size.y:
+	if position.y + collision_height >= screen_size.y:
 		pass
 		print("dead")
 
