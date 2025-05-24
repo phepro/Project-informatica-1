a984243f26d497039e6bc5fa18be03942aadc018
phepro <vojtasmit2009@gmail.com>
2025-05-25 00:54:49 +0200

    wwise shit

---
+++ Game/Wwise/resources/wwise_ids.gd
@@ -0,0 +1,26 @@
+class_name AK
+
+class EVENTS:
+	pass
+
+class STATES:
+	pass
+
+class SWITCHES:
+	pass
+
+class GAME_PARAMETERS:
+	pass
+
+class TRIGGERS:
+	pass
+
+class BANKS:
+	pass
+
+class AUX_BUSSES:
+	pass
+
+class ACOUSTIC_TEXTURES:
+	pass
+
